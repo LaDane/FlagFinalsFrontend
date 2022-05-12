e827a18cb33b1d14c3d195937fc8e04037909e5d
Eske Jayaswal <cph-ej162@cphbusiness.dk>
2022-05-12 14:36:01 +0200

    tilføjet flere kontinenter til quizzen

--- src/components/Quiz/Lobby.jsx
+++ src/components/Quiz/Lobby.jsx
@@ -2,64 +2,73 @@ import React from "react";
 import "./Quiz.css";
 
 const Lobby = ({ setStage, generateQuiz }) => {
-	const startQuiz = (evt, continent) => {
-		evt.preventDefault();
-		setStage(1);
-		generateQuiz(continent);
-	};
+  const startQuiz = (evt, continent) => {
+    evt.preventDefault();
+    setStage(1);
+    generateQuiz(continent);
+  };
 
-	return (
-		<div className="background">
-			<div className="center-items">
-				<h2 className="game-mode">Pick a game mode!</h2>
-				<button
-					className="lobby-start-btn"
-					onClick={(e) => {
-						startQuiz(e, "Europe");
-					}}
-				>
-					Europe
-				</button>
+  return (
+    <div className="background">
+      <div className="center-items">
+        <h2 className="game-mode">Pick a game mode!</h2>
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "Europe");
+          }}
+        >
+          Europe
+        </button>
 
-				<button
-					className="lobby-start-btn"
-					onClick={(e) => {
-						startQuiz(e, "Asia");
-					}}
-				>
-					Asia
-				</button>
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "Asia");
+          }}
+        >
+          Asia
+        </button>
 
-				<button
-					className="lobby-start-btn"
-					onClick={(e) => {
-						startQuiz(e, "North America");
-					}}
-				>
-					North America
-				</button>
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "North America");
+          }}
+        >
+          North America
+        </button>
 
-				<button
-					className="lobby-start-btn"
-					onClick={(e) => {
-						startQuiz(e, "Africa");
-					}}
-				>
-					Africa
-				</button>
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "South America and Oceania");
+          }}
+        >
+          South America and Oceania
+        </button>
 
-				<button
-					className="lobby-start-btn"
-					onClick={(e) => {
-						startQuiz(e, "World");
-					}}
-				>
-					World
-				</button>
-			</div>
-		</div>
-		// <p className="how-to-play">Rules and how to play / earn points</p>
-	);
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "Africa");
+          }}
+        >
+          Africa
+        </button>
+
+        <button
+          className="lobby-start-btn"
+          onClick={(e) => {
+            startQuiz(e, "World");
+          }}
+        >
+          World
+        </button>
+      </div>
+    </div>
+    // <p className="how-to-play">Rules and how to play / earn points</p>
+  );
 };
 
 export default Lobby;
