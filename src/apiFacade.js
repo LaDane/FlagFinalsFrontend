b75b8152297e229fd3aef788fb448f9e4f6ca44c
Aleksander W <us.aw@hotmail.com>
2022-05-19 11:27:02 +0200

    Update apiFacade.js

--- src/apiFacade.js
+++ src/apiFacade.js
@@ -1,4 +1,4 @@
-const URL = "https://alekw.dk/flagfinals";
+const URL = "https://alekw.dk/ffBackend";
 // const URL = "http://localhost:8080/FlagFinalsBackend_war_exploded";
 
 function handleHttpErrors(res) {
